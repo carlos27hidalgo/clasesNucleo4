87d4ba8bbea09daeb97d685f394246267c30416c
carlos27hidalgo <carlos27hidalgo@gmail.com>
2024-04-09 20:55:13 -0500

    segundo commit

--- src/app/pages/detalle-proyecto/detalle-proyecto.component.ts
+++ src/app/pages/detalle-proyecto/detalle-proyecto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-proyecto',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class DetalleProyectoComponent {
 
+  proyectos=[
+    {
+      id:1, nombre:"proyecto aplicacion web1", 
+      url:"https://github.com/proyecto1"
+    }, 
+    {
+      id:2, nombre:"proyecto aplicacion movil", 
+      url:"https://github.com/proyecto2"
+    }, 
+    {
+      id:3, nombre:"proyecto aplicacion spring", 
+      url:"https://github.com/proyecto3"
+    },
+    {
+      id:4, nombre:"proyecto aplicacion base de datos", 
+      url:"https://github.com/proyecto4"
+    },
+    {
+      id:5, nombre:"proyecto aplicacion BDD", 
+      url:"https://github.com/proyecto5"
+    },
+    {
+      id:6, nombre:"proyecto aplicacion React native", 
+      url:"https://github.com/proyecto6"
+    }
+  ]
+
+  constructor(private ruta: ActivatedRoute){
+
+  }
+
+  proyecto: any;
+
+  ngOnInit(){
+    this.ruta.params.subscribe(p => {
+      console.log(this.proyectos.find(proyecto => proyecto.id== p['idproyectos'] ))
+      this.proyecto = this.proyectos.find(proyecto => proyecto.id== p['idproyectos'] )
+    })
+  }
 }
